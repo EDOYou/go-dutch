a9efe4e6d02445f9a7da302f25d78efb52ae3bbe
Taghiyev <ktaghiyev01@gmail.com>
2022-11-26 18:52:28 +0100

    Go-Dutch

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,23 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt(); // Total amount of bill
+        int friends = sc.nextInt(); // Number of friends
+        int bill; // Bill to be paid, including tips
+        int foreach; // Bill after splitting (equally) amongst friends
+
+        if (total < 0) System.out.println("Bill total amount cannot be negative");
+        else if (friends <= 0) System.out.println("Number of friends cannot be negative or zero");
+        else {
+            bill = total + total * 10 / 100;
+            foreach = bill / friends;
+            System.out.println(foreach);
+        }
     }
 }
